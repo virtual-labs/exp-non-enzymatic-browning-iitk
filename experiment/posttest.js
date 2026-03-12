d5e582fbc1a370ed21b2ad96143cb6f0d7bee8cb
IIT Kanpur-Virtual Lab <74287892+vlabiitk@users.noreply.github.com>
2026-03-12 10:48:31 +0530

    Update posttest.js

--- experiment/posttest.js
+++ experiment/posttest.js
@@ -131,7 +131,20 @@
         d: "None of the above"                   ///// Write the option 4 inside double quotes
       },
       correctAnswer: "b"                ///// Write the correct option inside double quotes
-    },                                  ///// To add more questions, copy the section below 
+    }, 
+    
+    {
+      question: "During centrifugation, suspended particles:",  ///// Write the question inside double quotes
+      answers: {
+        a: "Float to the surface",                  ///// Write the option 1 inside double quotes
+        b: "Settle at the bottom due to centrifugal force",                  ///// Write the option 2 inside double quotes
+        c: "Remain uniformly distributed",                  ///// Write the option 3 inside double quotes
+        d: "Dissolve completely in the medium"                   ///// Write the option 4 inside double quotes
+      },
+      correctAnswer: "b"                ///// Write the correct option inside double quotes
+    }, 
+    
+    ///// To add more questions, copy the section below 
 
 
     /* To add more MCQ's, copy the below section, starting from open curly braces ( { )
